7ac8aea89565644003471ae15cf194d93eb6b16e
Adeboga Abigail <abbyboga@gmail.com>
2024-02-26 04:53:08 -0800

    Finally!!!

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -26,6 +26,7 @@ void swap_digits(int *array, int i, int j)
  * @array: array to partion
  * @lower: lower bound of partition
  * @upper: upper bound of partition
+ * @size: size of array
  *
  * Return: an integer that indicates the end of the partition
  */
@@ -33,21 +34,27 @@ void swap_digits(int *array, int i, int j)
 int partition_quick_sort(int *array, int lower, int upper, size_t size)
 {
 	int pivot = array[upper];
-	int start = lower - 1;
+	int start = lower;
 	int end;
 
-	for (end = lower; end <= upper - 1; end++)
+	for (end = lower; end < upper; end++)
 	{
 		if (array[end] <= pivot)
 		{
+			if (start != end)
+			{
+				swap_digits(array, start, end);
+				print_array(array, size);
+			}
 			start++;
-			swap_digits(array, start, end);
 		}
-
-	}		
-	swap_digits(array, start + 1, upper);
-	print_array(array, size);
-	return (start + 1);
+	}
+	if (start < upper)
+	{
+		swap_digits(array, start, upper);
+		print_array(array, size);
+	}
+	return (start);
 }
 
 /**
@@ -55,22 +62,23 @@ int partition_quick_sort(int *array, int lower, int upper, size_t size)
  * @arr: array to sort
  * @lower: starting index
  * @upper: index to end at
+ * @size: size of array
  */
 
 void recursive_quick_sort(int *arr, int lower, int upper, size_t size)
 {
-	int location;
-
 	if (lower < upper)
 	{
-		location = partition_quick_sort(arr, lower, upper, size);
+		int location = partition_quick_sort(arr, lower, upper, size);
+
 		recursive_quick_sort(arr, lower, location - 1, size);
 		recursive_quick_sort(arr, location + 1, upper, size);
 	}
 
 }
 /**
- * quick_sort - sort an array of integers in ascending order using the quick sort algorithm
+ * quick_sort - sort an array of integers in ascending order
+ * using the quick sort algorithm
  * @array: array of integers to sort
  * @size: size of array
  *
