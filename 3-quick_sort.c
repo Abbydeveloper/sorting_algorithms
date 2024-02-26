49ebb4784fe12ede40b390f95fcef06552c045be
Adeboga Abigail <abbyboga@gmail.com>
2024-02-26 03:12:43 -0800

    Fix?

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,36 +1,54 @@
 #include "sort.h"
 
 /**
+ * swap_digits - swap two digits within an array
+ * @array: array to swap items in
+ * @i: index of the first digit to swap
+ * @j: index of the second digit to swap
+ *
+ * Return: Nothing
+ */
+
+void swap_digits(int *array, int i, int j)
+{
+	int tmp;
+
+	if (array == NULL)
+		return;
+	tmp = array[i];
+	array[i] = array[j];
+	array[j] = tmp;
+}
+
+
+/**
  * partition_quick_sort - partition a quick sort array
  * @array: array to partion
  * @lower: lower bound of partition
  * @upper: upper bound of partition
  *
- * Return: and integer that indicates the end of the partition
+ * Return: an integer that indicates the end of the partition
  */
 
 int partition_quick_sort(int *array, int lower, int upper, size_t size)
 {
-	int pivot = array[lower];
+	int pivot = array[upper];
 	int start = lower - 1;
-	int end = upper + 1;
-	int tmp;
+	int end;
 
-	while (1)
+	for (end = lower; end <= upper - 1; end++)
 	{
-		while (array[start] < pivot)
+		if (array[end] <= pivot)
+		{
 			start++;
-		while (array[end] > pivot)
-			end--;
+			swap_digits(array, start, end);
+			print_array(array, size);
+		}
 
-		if (start >= end)
-			return end;
-		
-		tmp = array[start];
-		array[start] = array[end];
-		array[end] = tmp;
-		print_array(array, size);
-	}
+	}		
+	swap_digits(array, start + 1, upper);
+	print_array(array, size);
+	return (start + 1);
 }
 
 /**
@@ -47,7 +65,7 @@ void recursive_quick_sort(int *arr, int lower, int upper, size_t size)
 	if (lower < upper)
 	{
 		location = partition_quick_sort(arr, lower, upper, size);
-		recursive_quick_sort(arr, lower, location, size);
+		recursive_quick_sort(arr, lower, location - 1, size);
 		recursive_quick_sort(arr, location + 1, upper, size);
 	}
 
@@ -61,5 +79,7 @@ void recursive_quick_sort(int *arr, int lower, int upper, size_t size)
 
 void quick_sort(int *array, size_t size)
 {
+	if (array == NULL)
+		return;
 	recursive_quick_sort(array, 0, size - 1, size);
 }
